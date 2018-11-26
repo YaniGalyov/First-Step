5990705f09d839813ae05271595d5b53c589843e
Yani <yani.galiov@gmail.com>
2018-11-26 12:24:06 +0200

    simplified

--- Greetings.go
+++ Greetings.go
@@ -4,10 +4,10 @@ import "fmt"
 
 func main() {
 
-    names := []string{"Yani", "Vlad", "Krasi", "Ivan", "Kris", "Naso", "Dimi", "Geri", "Lili", "Zoya"}
+	names := []string{"Yani", "Vlad", "Krasi", "Ivan", "Kris", "Naso", "Dimi", "Geri", "Lili", "Zoya"}
 
-    for i, c := range names {
-        fmt.Println("", i)
-        fmt.Println("", c)
-    }
+	for _, c := range names {
+		fmt.Println("Name:", c)
+	}
+
 }
