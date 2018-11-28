53339276487ea8cf3fb5003083a4320446767fd9
Yani <yani.galiov@gmail.com>
2018-11-28 23:48:39 +0200

    closed #2

--- Hello.go
+++ Hello.go
@@ -2,6 +2,17 @@ package main
 
 import "fmt"
 
-func main() {  
-    fmt.Println("First step")
+func main() {
+	printNames()
+}
+
+func printNames() {
+
+	fmt.Println("First-Step")
+
+	names := []string{"Yani", "Vlad", "Krasi", "Ivan", "Kris", "Naso", "Dimi", "Geri", "Lili", "Zoya"}
+
+	for _, c := range names {
+		fmt.Println("Name:", c)
+	}
 }
